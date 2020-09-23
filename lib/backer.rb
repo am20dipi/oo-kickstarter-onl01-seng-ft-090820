e50a33548d9b31690aecda8c3c724e3a37671f2c
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-23 22:16:23 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,8 @@
 class Backer
   attr_reader :backed_projects, :name
   
-  def initialize(backed_projects)
+  def initialize(name,backed_projects)
+    @name = name
     @backed_projects =[]
   end
   
